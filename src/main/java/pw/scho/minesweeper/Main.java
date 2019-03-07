a255ad0af07a380f082a7cfd6cd56b366d612cb7
Georg Meyer <georg@scho.pw>
2019-03-07 21:22:31 +0100

    Clean up game loop

--- src/main/java/pw/scho/minesweeper/Main.java
+++ src/main/java/pw/scho/minesweeper/Main.java
@@ -1,58 +1,71 @@
 package pw.scho.minesweeper;
 
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Main {
 
     private Game game = Game.newDefault();
 
     private void run() {
-        loop();
+        printGameState();
 
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            Position position = Position.of(sc.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Position position = readPosition(scanner);
 
-            System.out.println("Reveal or mark as bomb (R/B):");
+            readAction(scanner).accept(position);
 
-            while (sc.hasNextLine()) {
-                String revealOrMarkAsBomb = sc.nextLine();
+            printGameState();
 
-                if (revealOrMarkAsBomb.equals("R")) {
-                    game.reveal(position);
-                    break;
-                }
-                if (revealOrMarkAsBomb.equals("B")) {
-                    game.markAsBomb(position);
-                    break;
-                }
-                throw new IllegalArgumentException("Must be R or B");
+            if (game.isGameOver()) {
+                System.out.println("* * * GAME OVER * * *");
+                break;
             }
 
-            if (!loop()) {
-                return;
+            if (game.isCorrect()) {
+                System.out.println("* * * YOU WON * * *");
+                break;
             }
         }
     }
 
-    private boolean loop() {
+    private Consumer<Position> readAction(Scanner scanner) {
+        System.out.println("Reveal or mark as bomb (R/B):");
+        while (scanner.hasNextLine()) {
+            String actionString = scanner.nextLine();
+
+            if (actionString.equals("R")) {
+                return game::reveal;
+            }
+            if (actionString.equals("B")) {
+                return game::reveal;
+            }
+            System.out.println("Must be R or B, please type in again (R/B):");
+        }
+        throw new IllegalStateException();
+    }
+
+    private Position readPosition(Scanner scanner) {
+        System.out.println("Select position:");
+        while (scanner.hasNextLine()) {
+            try {
+                Position position = Position.of(scanner.nextLine());
+
+                if (position.isValid()) {
+                    return position;
+                }
+                System.out.println("Position is invalid, please type in again:");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Position is invalid, please type in again:");
+            }
+        }
+        throw new IllegalStateException();
+    }
+
+    private void printGameState() {
         game.print(System.out::println);
         System.out.println();
-
-        if (game.isGameOver()) {
-            System.out.println("* * * GAME OVER * * *");
-            return false;
-        }
-
-        if (game.isCorrect()) {
-            System.out.println("* * * YOU WON * * *");
-            return false;
-        }
-
-
-        System.out.println("Select position:");
-
-        return true;
     }
 
     public static void main(String[] args) {
